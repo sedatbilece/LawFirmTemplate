5c0fcfad032d61923ec45ac4f6c77c5e501063b5
sedatbilece <seda1tbilece@gmail.com>
2023-11-04 18:06:13 +0300

    fix Home index and layout

--- LawFirmTemplate/Controllers/HomeController.cs
+++ LawFirmTemplate/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using LawFirmTemplate.Models;
+﻿using LawFirmTemplate.Data;
+using LawFirmTemplate.Models;
+using LawFirmTemplate.Models.Home;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,9 +9,23 @@ namespace LawFirmTemplate.Controllers
     public class HomeController : Controller
     {
 
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()//test
         {
-            return View();
+            var model = new HomeIndexModel()
+            {
+                clientSays = _context.ClientSays.OrderBy(x => x.Order).ToList(),
+                teams = _context.Users.OrderBy(x => x.Order).Where(x => x.RoleType == Data.Enums.RoleType.Normal).ToList(),
+                firm = _context.Firms.FirstOrDefault(),
+            };
+            
+            return View(model);
         }
 
        
--- LawFirmTemplate/Models/Home/HomeIndexModel.cs
+++ LawFirmTemplate/Models/Home/HomeIndexModel.cs
@@ -1,6 +1,14 @@
-﻿namespace LawFirmTemplate.Models.Home
+﻿using LawFirmTemplate.Data.Entities;
+
+namespace LawFirmTemplate.Models.Home
 {
     public class HomeIndexModel
     {
+
+        public List<ClientSays> clientSays { get; set; }
+        public List<Data.Entities.User> teams { get; set; }
+        public Firm firm { get; set; }
+
+
     }
 }
